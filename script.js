9ef8136d6cd4e1ce166edeacdfa2f28f588b51c7
A-Dummo <2014adamw@gmail.com>
2022-02-22 00:34:37 -0800

    made grid divs stretch

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function run() {
     const percentage = 1 / n * 100;
     for (let i = 0; i < n * n; i++) {
         //grid.innerHTML += "<div class='etchPixel'><p>f</p></div>";
-        grid.innerHTML += `<div style="min-width: ${percentage}%; min-height: ${percentage}%"><p>f</p></div>`;
+        //grid.innerHTML += `<div style="min-width: ${percentage}%; min-height: ${percentage}%"><p>f</p></div>`;
+        grid.innerHTML += `<div class='etchPixel' style="min-width: ${percentage}%"><p>f</p></div>`;
     }
 }
 
