6f694eb83c582522a9ca77e459a319c4fe2b4fd2
A-Dummo <2014adamw@gmail.com>
2022-02-22 00:11:38 -0800

    added button and prompt

---
+++ script.js
@@ -0,0 +1,11 @@
+function run() {
+    const grid = document.querySelector("#grid-of-divs");
+    const n = Number(prompt());
+    const percentage = 1 / n * 100;
+    for (let i = 0; i < n * n; i++) {
+        //grid.innerHTML += "<div class='etchPixel'><p>f</p></div>";
+        grid.innerHTML += `<div style="min-width: ${percentage}%; min-height: ${percentage}%"><p>f</p></div>`;
+    }
+}
+
+run();
