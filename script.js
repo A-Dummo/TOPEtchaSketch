ce473a6bc954445baaa9fd3109c38c086283c1df
A-Dummo <2014adamw@gmail.com>
2022-02-22 01:04:18 -0800

    made pixel turn lightgray on hover

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ function run() {
     const n = Number(prompt());
     const percentage = 1 / n * 100;
     for (let i = 0; i < n * n; i++) {
-        //grid.innerHTML += "<div class='etchPixel'><p>f</p></div>";
-        //grid.innerHTML += `<div style="min-width: ${percentage}%; min-height: ${percentage}%"><p>f</p></div>`;
-        grid.innerHTML += `<div class='etchPixel' style="min-width: ${percentage}%"><p>f</p></div>`;
+        grid.innerHTML += `<div class="etchPixel" style="min-width: ${percentage}%"></div>`;
     }
 }
 
 run();
+
+const grids = document.querySelectorAll(".etchPixel");
+grids.forEach(item => {
+    item.addEventListener('mouseover', event => {
+        item.classList.add("pixelGray");
+        console.log(1);
+    });
+});
